c8ee60dfd453bf55aaeb555a6656d67979ade1b0
Mitchell Fagert <mitchell.fagert@gmail.com>
2022-08-29 15:15:38 -0400

    Uploading add'l changes.

--- static/js/apps.js
+++ static/js/apps.js
@@ -45,5 +45,8 @@ function handleClick() {
     // just be the original tableData.
     buildTable(filteredData);
 };
-
+// Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
